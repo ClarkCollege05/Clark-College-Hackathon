1ae09dc8854da00846fe0865e77b4fffcb5f07c8
ClarkCollege05 <alexander2021mercado@gmail.com>
2022-04-08 04:42:30 +0000

    initial commit

--- src/index.py
+++ src/index.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('employee.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
