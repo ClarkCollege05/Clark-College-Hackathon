be081571e8659a62f7519a672e822278bff42313
ClarkCollege05 <alexander2021mercado@gmail.com>
2022-04-08 09:42:02 +0000

    initial commit

--- src/index.py
+++ src/index.py
@@ -27,6 +27,8 @@ def index():
         cur.execute("INSERT INTO employee(firstname, lastname,position,SickLeaveCredits,VacationLeave,hourlyRate) VALUES(%s, %s,%s, %s, %s,%s)",(name,lastname,position,credits,VacationL,hourly))
         mysql.connection.commit()
         cur.close()
+        TY = 'Registered Successfully!'
+        return render_template('index.html',TY=TY)
     return render_template('index.html')
 
 
@@ -69,7 +71,37 @@ def list_empl():
         
     return render_template('employee_list.html',TEXT=TEXT,result=result)
 
+
+@app.route("/delete_admin",  methods = ['GET',"POST"])
+def Delete_ADMIN():
+
+    cur = mysql.connection.cursor()
+    results = cur.execute("SELECT * FROM admin")
+
+
+    if results > 0:
+        result = cur.fetchall()
+        TEXT = 'DELETE ADMIN' 
+        if request.method == 'POST':
+
+
+            IDs = request.form["IDs"]    
+            curs = mysql.connection.cursor()
+            curs.execute("DELETE FROM admin WHERE id = '"+IDs+"'")
+            mysql.connection.commit()
+            curs.close()
+
+
+            return redirect('/delete_admin')
+
+
             
+        return render_template('delete_admin.html',TEXT=TEXT,result=result)
+    else:
+        TEXT = 'EMPTY'
+        return render_template('delete_admin.html',TEXT=TEXT)
+        
+    return render_template('delete_admin.html',TEXT=TEXT,result=result)
 
 
     
@@ -108,25 +140,83 @@ def Landing():
     return render_template("landing.html")
 
 
-@app.route("/LogIn_admin",  methods = ['GET',"POST"])
-def AdminLog():
-    return render_template("Admin_login.html")
+
+@app.route("/ADMIN_list",  methods = ['GET',"POST"])
+def list_ADD():
+
+    cur = mysql.connection.cursor()
+    results = cur.execute("SELECT * FROM admin")
 
 
-@app.route("/AdminMain",  methods = ['GET',"POST"])
-def AdminLogMAIN():
-    return render_template("MainAdmin.html")
+    if results > 0:
+        result = cur.fetchall()
+        TEXT = "ADMIN LIST"
+        return render_template('admins_list.html',result=result,TEXT=TEXT)
+    else:
+        resultt = "empty"
+        TEXT = "EMPTY"
+        return render_template('admins_list.html',resultt=resultt,TEXT=TEXT)
+
+
+@app.route("/AdminMain", methods = ['GET',"POST"])
+def loginADMINN():
+    
+    if request.method == 'POST':
+        username = request.form["email"]
+        Password = request.form["password"]
+
+              
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM Mainadmin WHERE email =  '"+username+"'")
+  
+        result = cur.fetchone()
+        
+        
+    
+        if result:
+            OrigPass = result['password']
+            if Password != OrigPass:
+                NotFound = "Wrong Password!"
+                return render_template('MainAdmin.html',NotFound=NotFound)
+
+            return redirect('/ADMIN_list')
+        else:
+            NotFound = "Email not found!"
+            return render_template('MainAdmin.html',NotFound=NotFound)
+
+    return render_template('MainAdmin.html')
+
+@app.route("/AddAddmin", methods = ['GET',"POST"])
+def ADdmin():
+
+    if request.method == 'POST':
+        usernames = request.form
+        name = usernames['firstName']
+        cur = mysql.connection.cursor()
+        lastname = usernames['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        clearance = request.form['clearance']
+
+        cur.execute("INSERT INTO admin(firstname, lastname,email,password,clearance) VALUES(%s, %s,%s, %s, %s)",(name,lastname,email,password,clearance))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/ADMIN_list')
+    return render_template('ADDadmin.html')
 
 
 
 
 
-@app.route("/loginADMIN", methods = ['GET',"POST"])
+
+
+
+@app.route("/LogIn_admin", methods = ['GET',"POST"])
 def loginADMIN():
     
     if request.method == 'POST':
         username = request.form["email"]
-        Password = request.form["Password"]
+        Password = request.form["password"]
 
               
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
